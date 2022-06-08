6030225beadedc7800ccbcc8a3aebe2fc167354f
Osagie007 <67745253+Osagie007@users.noreply.github.com>
2022-06-08 13:25:54 +0100

    updated urls

--- Rizzz/urls.py
+++ Rizzz/urls.py
@@ -19,10 +19,14 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
  ]
  
 urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
