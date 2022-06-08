51db01232e273dfe6faf3854eab82c97be1202e5
Osagie007 <eluojerio.osagie@yahoo.com>
2022-06-08 15:11:11 +0100

    third commit

--- Rizzz/settings.py
+++ Rizzz/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Quick-start development settings - unsuitable for production
