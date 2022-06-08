3880b08fda85229bd4b9bd5b6e4c413a4f594dfb
Osagie007 <eluojerio.osagie@yahoo.com>
2022-06-08 15:23:11 +0100

    third commit

--- Rizzz/urls.py
+++ Rizzz/urls.py
@@ -20,13 +20,13 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 from django.views.static import serve
-from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path('media/(<path>.*)', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    path('static/(<path>.*)', serve,{'document_root': settings.STATIC_ROOT}), 
  ]
  
 urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
